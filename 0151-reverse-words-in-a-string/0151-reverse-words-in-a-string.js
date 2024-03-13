5d4b4bbbbace915ffdd4d892f02c192b085e61f4
Yosyp Dobosh <98188386+dobosh28@users.noreply.github.com>
2024-03-12 21:14:55 -0400

    Time: 56 ms (52.69%), Space: 51.8 MB (9.31%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,12 +3,18 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    let words = s.trim().split(/\s+/);
-    let reversedWords = [];
+    let result = '';
+    let i = 0;
+    const n = s.length;
     
-    for (let i = words.length - 1; i >= 0; i--) {
-        reversedWords.push(words[i]);
+    while (i < n) {
+        while ( i < n && s[i] === ' ') i++;
+        if (i >= n) break;
+        let j = i + 1;
+        while ( j < n && s[j] !== ' ') j++;
+        const sub = s.substring(i, j);
+        result = result.length === 0 ? sub : sub + ' ' + result;
+        i = j + 1;
     }
-    
-    return reversedWords.join(" ");
+    return result;
 };
